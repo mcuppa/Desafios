9a0bd131057dca8e55af07cdb02a0bd1f6f6d8d2
mcuppa <marianocuppari@gmail.com>
2021-11-23 17:24:22 -0300

    Desafio componentes 2

--- desafio-3/mi-ecommerce-2/src/App.js
+++ desafio-3/mi-ecommerce-2/src/App.js
@@ -5,17 +5,36 @@ import "./components/bootstrap.min.css";
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
+
+
 function App() {
   return (
     <div>
       <NavBar />
+      <div className="container">
      <ItemListContainer titulo="producto1" descripcion="
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam condimentum ultricies est, ut bibendum leo efficitur vitae. Duis dignissim vitae sapien iaculis lobortis." />
      <ItemListContainer titulo="producto2" descripcion="
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam condimentum ultricies est, ut bibendum leo efficitur vitae. Duis dignissim vitae sapien iaculis lobortis." />
      <ItemListContainer titulo="producto3" descripcion="
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam condimentum ultricies est, ut bibendum leo efficitur vitae. Duis dignissim vitae sapien iaculis lobortis." />
-
+      </div>
+      <div className="container">
+     <ItemListContainer titulo="producto1" descripcion="
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam condimentum ultricies est, ut bibendum leo efficitur vitae. Duis dignissim vitae sapien iaculis lobortis." />
+     <ItemListContainer titulo="producto2" descripcion="
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam condimentum ultricies est, ut bibendum leo efficitur vitae. Duis dignissim vitae sapien iaculis lobortis." />
+     <ItemListContainer titulo="producto3" descripcion="
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam condimentum ultricies est, ut bibendum leo efficitur vitae. Duis dignissim vitae sapien iaculis lobortis." />
+      </div>
+      <div className="container">
+     <ItemListContainer titulo="producto1" descripcion="
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam condimentum ultricies est, ut bibendum leo efficitur vitae. Duis dignissim vitae sapien iaculis lobortis." />
+     <ItemListContainer titulo="producto2" descripcion="
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam condimentum ultricies est, ut bibendum leo efficitur vitae. Duis dignissim vitae sapien iaculis lobortis." />
+     <ItemListContainer titulo="producto3" descripcion="
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam condimentum ultricies est, ut bibendum leo efficitur vitae. Duis dignissim vitae sapien iaculis lobortis." />
+      </div>
     </div>
   );
 }
